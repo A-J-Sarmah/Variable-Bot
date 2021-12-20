91885f8ee3e2cd9fe561d9e851752997a5b26bf7
ajsarmah <thisisamlan49@gmail.com>
2021-12-20 14:13:01 +0530

    Added LeetCode API support and created necessary Utility functions

--- bot.js
+++ bot.js
@@ -1,16 +1,34 @@
 require("dotenv").config();
 
+const { default: axios } = require("axios");
 const { Client, Intents } = require("discord.js");
 const { DISCORD_BOT_TOKEN } = process.env;
 const variables = require("./config");
+const prefix = "!problem";
+const leetCodeApiURL = "https://leetcode.com/api/problems/all/";
+const utils = require("./utils");
+const problems = [];
+let totalProblemGenerated = 0;
 
 // CLIENT CREATION
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+//IMPORTING PROBLEMS
+axios.get(leetCodeApiURL).then((response) => {
+  response.data.stat_status_pairs.forEach((problem) => {
+    const newProblem = new utils.LeetCodeProblem(problem); //creates new problems with the help of the constructor imported from utils
+    problems.push(newProblem);
+  });
+  totalProblemGenerated = problems.length;
+  console.log(problems);
+});
 
 // BOT GETS READY
 client.on("ready", () => {
   console.log("Bot is Online and Responsive");
 });
+
+client.on("message", (message) => {});
 
 // GUILD CREATION
 client.on("guildCreate", (guild) => {
@@ -18,7 +36,6 @@ client.on("guildCreate", (guild) => {
   let name = guild.name;
 
   //   Change Bot Nickname for the server to -> (name + Bot)
-  
 });
 
 // GIVES BOT A ROLE
---
+++ utils.js
@@ -0,0 +1,44 @@
+//Creating Problem Constructor
+module.exports.LeetCodeProblem = function (problemObject) {
+  this.id = problemObject.stat.question_id;
+  this.title = problemObject.stat.question__title;
+  this.difficulty =
+    problemObject.difficulty.level === 3
+      ? "Hard"
+      : problemObject.difficulty.level === 2
+      ? "Medium"
+      : "Easy";
+  this.paidOnly = problemObject.paid_only;
+  this.titleSlug = problemObject.stat.question__title_slug;
+  this.description = `Problem ID: ${this.id}\nTitle: ${this.title}\nSlug Title: ${this.titleSlug}\nDifficulty: ${this.difficulty}\nIs Paid? ${this.paidOnly}`;
+};
+
+module.exports.generateRandomIndex = function (length) {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+module.exports.sendProblem = function (
+  message,
+  difficulty = " ",
+  index,
+  problems
+) {
+  if (difficulty !== " ") {
+    problems = problems.filter((problem) => {
+      problem.difficulty.toLowerCase() === diff;
+    });
+  }
+  const problem = problem[index];
+  const problemURL = "https://leetcode.com/problems/" + problem.titleSlug + "/";
+  const msg = new MessageEmbed()
+    .setTitle(problem.title)
+    .setColor("#f89f1b")
+    .setThumbnail("https://leetcode.com/static/images/LeetCode_logo_rvs.png")
+    .setDescription(
+      `${problem.difficulty} ${
+        problem.paidOnly ? "locked/paid" : "unlocked/free"
+      } problem.`
+    )
+    .setURL(problemURL);
+  message.channel.send(msg);
+};
